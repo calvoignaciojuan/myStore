3b3e83e5b69532f7aceb4f2a9a631949170954d0
calvoignaciojuan <calvoignaciojuan@gmail.com>
2023-04-22 17:08:27 -0300

    up5

--- index.js
+++ index.js
@@ -4,24 +4,26 @@ const {logErrors,boomErrorHandler,errorHandler} = require("./middlewares/error.h
 const cors = require("cors");
 
 const app = express();
-const port = 80;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json()); //para que me puedan llegar objetos json por POST, sino no req.body no devuelve nada
 
 app.use(cors());
 
+
+
+routerApi(app);
+
 app.use("/",(req,res)=>{
   res.send("HOLA");
 });
-
-routerApi(app);
 
 app.use(logErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
-app.listen(port,()=>{
-  console.log(`server listening in http://localhost:${port}`);
+app.listen(PORT,()=>{
+  console.log(`server listening in http://localhost:${PORT}`);
 });
 
 
